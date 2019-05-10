385dd5ff8f6008e7fb6e764ac62aa477ac1040e0
stanislawbartkowski@gmail.com <stanislawbartkowski@gmail.com>
2019-05-10 14:02:59 +0200

    changes

--- sh.templates/run.sh
+++ sh.templates/run.sh
@@ -1,14 +1,14 @@
 export TEMPDIR=/tmp/mytpcds
 #export TCPROOT=/home/sbartkowski/work/v2.10.0rc2
-export TCPROOT=/home/perf/v2.10.1rc3
-export TCPDATA=/mnt/data/data
+export TCPROOT=/home/perf/v2.11.0rc2
+#export TCPDATA=/mnt/data/data
 
 #export ENV=env/db2
 #export ENV=env/bigsql
-#export ENV=env/oracle
+export ENV=env/oracle
 #export ENV=env/hive
 #export ENV=env/postgresql
-export ENV=env/sparksql
+#export ENV=env/sparksql
 #export ENV=env/thrive
 #export ENV=env/phoenix
 #export ENV=env/mysql
@@ -17,15 +17,16 @@ export ENV=env/sparksql
 
 
 export TESTDATA=call_center
+#export TESTDATA=dbgen_version
 #export TESTDATA=catalog_sales
 #export TESTDATA=store_sales
 #export TESTDATA=dbgen_version
 
-export TESTQUERY=01
+export TESTQUERY=32
 #export QUERYTIMEOUT=5m
 #export QUERYTIMEOUT=1m
 
-export DONOTVERIFY=X
+#export DONOTVERIFY=X
 export QUERYTIMEOUT=59m
 
 #./tpc.sh test
@@ -33,9 +34,9 @@ export QUERYTIMEOUT=59m
 #./tpc.sh createtables
 #./tpc.sh loadtest
 #./tpc.sh testverify
-./tpc.sh testquery
+#./tpc.sh testquery
 
 #./tpc.sh loaddata
 #./tpc.sh verifyload
-#./tpc.sh runqueries
+./tpc.sh runqueries
 if [ $? -ne 0 ]; then echo "FAILED"; else echo "PASSED"; fi
