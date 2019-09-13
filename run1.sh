1a7d658c1c42e4ba93a96955428fdbed0a1df993
perf perf <perf@glede1.fyre.ibm.com>
2019-09-13 16:25:05 +0200

    changes

--- run1.sh
+++ run1.sh
@@ -4,11 +4,11 @@ export TCPROOT=/home/perf/v2.11.0rc2
 export TCPDATA=/mnt/data/data1
 
 #export ENV=env/db2
-export ENV=env/bigsql
+#export ENV=env/bigsql
 #export ENV=env/oracle
 #export ENV=env/hive
 #export ENV=env/postgresql
-#export ENV=env/sparksql
+export ENV=env/sparksql
 #export ENV=env/thrive
 #export ENV=env/phoenix
 #export ENV=env/mysql
@@ -22,7 +22,7 @@ export TESTDATA=call_center
 #export TESTDATA=store_sales
 #export TESTDATA=dbgen_version
 
-export TESTQUERY=86
+export TESTQUERY=32
 #export QUERYTIMEOUT=5m
 #export QUERYTIMEOUT=1m
 
@@ -34,9 +34,9 @@ export QUERYTIMEOUT=59m
 #./tpc.sh createtables
 #./tpc.sh loadtest
 #./tpc.sh testverify
-#./tpc.sh testquery
+./tpc.sh testquery
 
 #./tpc.sh loaddata
 #./tpc.sh verifyload
-./tpc.sh runqueries
+#./tpc.sh runqueries
 if [ $? -ne 0 ]; then echo "FAILED"; else echo "PASSED"; fi
