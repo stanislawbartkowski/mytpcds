9abc31a1d894b56f368c26d4059c13adc1b01843
sbartkowski <stanislawbartkowski@gmail.com>
2021-12-25 18:39:48 +0100

    db2cliproc different load server directory

--- db/db2cliproc.sh
+++ db/db2cliproc.sh
@@ -1,11 +1,21 @@
 source proc/db2commonproc.sh
 
+#set -x
+#w
+
+
 loadfiles3() {
   db2loadfiles3 $1 $2
 }
 
 loadfileclient() {
-  db2loadfileserver $1 $2
+  if [ -n "$LOADSERVERDIR" ]; then
+    log "LOADSERVERDIR enabled : $LOADSERVERDIR"
+    log "Load file from $LOADSERVERDIR/$1.dat"
+     db2loadfileserver $1 $LOADSERVERDIR/$1.dat;
+  else 
+     db2loadfileserver $1 $2;
+  fi
 }
 
 
